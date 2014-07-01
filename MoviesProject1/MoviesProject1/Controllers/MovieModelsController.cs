485d6d1ff629b3c776cb72e91e8c9832f593e695
mwerner340 <mikewerner@att.net>
2014-07-01 11:41:21 -0400

    add movie view

---
+++ MoviesProject1/MoviesProject1/Controllers/MovieModelsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MoviesProject1.Models;
+
+namespace MoviesProject1.Controllers
+{
+    public class MovieModelsController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        // GET: MovieModels
+        public ActionResult Index()
+        {
+            return View(db.MovieModels.ToList());
+        }
+
+        // GET: MovieModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovieModel movieModel = db.MovieModels.Find(id);
+            if (movieModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movieModel);
+        }
+
+        // GET: MovieModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MovieModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genere,Price")] MovieModel movieModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MovieModels.Add(movieModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movieModel);
+        }
+
+        // GET: MovieModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovieModel movieModel = db.MovieModels.Find(id);
+            if (movieModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movieModel);
+        }
+
+        // POST: MovieModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genere,Price")] MovieModel movieModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movieModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movieModel);
+        }
+
+        // GET: MovieModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovieModel movieModel = db.MovieModels.Find(id);
+            if (movieModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movieModel);
+        }
+
+        // POST: MovieModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MovieModel movieModel = db.MovieModels.Find(id);
+            db.MovieModels.Remove(movieModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MoviesProject1/MoviesProject1/Models/MovieModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MoviesProject1.Models
+{
+    public class MovieModel
+    {
+            public int ID { get; set; }
+            public string Title { get; set; }
+            public DateTime ReleaseDate { get; set; }
+            public string Genere { get; set; }
+            public decimal Price { get; set; }
+        }
+        public class MovieDBContext : DbContext
+        {
+            public DbSet<MoviesProject1.Movies.Movie> Movies { get; set; }
+
+            public System.Data.Entity.DbSet<MoviesProject1.Models.MovieModel> MovieModels { get; set; }
+
+        }
+    }
+
+
+   
